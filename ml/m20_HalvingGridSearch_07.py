dbd472f769e0ba176901c52ec5de50718e589d2a
junhyeokk97 <junhyeok97@naver.com>
2025-09-10 18:03:00 +0900

    [102일차] ml / m20_HalvingGridSearch_07.py

---
+++ ml/m20_HalvingGridSearch_07.py
@@ -0,0 +1,133 @@
+import numpy as np
+from sklearn.datasets import load_breast_cancer
+from sklearn.model_selection import train_test_split,KFold,StratifiedKFold
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.metrics import accuracy_score
+from sklearn.svm import SVC
+from sklearn.model_selection import GridSearchCV,RandomizedSearchCV
+from sklearn.experimental import enable_halving_search_cv  # 이것을 먼저 임포트
+from sklearn.model_selection import HalvingGridSearchCV
+import time
+from xgboost import XGBClassifier,XGBRegressor
+import pandas as pd 
+from sklearn.metrics import r2_score
+
+#1. 데이터 
+path = './_data/dacon/당뇨병/dacon diabetes/'
+train_csv = pd.read_csv(path + 'train.csv', index_col=0)
+test_csv = pd.read_csv(path + 'test.csv', index_col=0)
+submit_csv = pd.read_csv(path + 'sample_submission.csv', index_col=0)
+
+cols_to_fix = ['Glucose', 'BloodPressure', 'SkinThickness', 'BMI', 'Insulin']
+
+# 0값을 NaN으로 대체
+train_csv[cols_to_fix] = train_csv[cols_to_fix].replace(0, np.nan)
+test_csv[cols_to_fix] = test_csv[cols_to_fix].replace(0, np.nan)
+
+# Feature Engineering (BMI * Age, Glucose / BMI)
+train_csv['BMI*Age'] = train_csv['BMI'] * train_csv['Age']
+train_csv['Glucose/BMI'] = train_csv['Glucose'] / (train_csv['BMI'] + 1e-5)
+test_csv['BMI*Age'] = test_csv['BMI'] * test_csv['Age']
+test_csv['Glucose/BMI'] = test_csv['Glucose'] / (test_csv['BMI'] + 1e-5)
+
+# Drop target before preprocessing
+x = train_csv.drop(['Outcome'], axis=1)
+y = train_csv['Outcome']
+
+
+# 3. 데이터 분할
+x_train, x_test, y_train, y_test = train_test_split(x, y, train_size=0.8, random_state=55)
+
+# (521, 10)
+
+from sklearn.preprocessing import StandardScaler
+scaler = StandardScaler()
+x_train = scaler.fit_transform(x_train)
+x_test = scaler.transform(x_test)
+
+
+
+n_split = 5 
+# kfold = KFold(n_splits=n_split, shuffle=True, random_state=333)
+kfold = StratifiedKFold(n_splits=n_split, shuffle=True, random_state=333)
+
+
+
+parameters =[
+    {'n_estimators' : [100,500],'max_depth':[6,10,12],'learning_rate':[0.1,0.01,0.001]}, # 12
+    {'max_depth':[6,8,10,12],'learning_rate':[0.1,0.01,0.001]},
+    {'min_child_weight':[2,3,5,10],'learning_rate':[0.1,0.01,0.001]}
+]
+
+#2. 모델
+xgb = XGBClassifier()
+# model = RandomizedSearchCV(xgb, parameters,cv = kfold, # 54 * 5 = 270번 
+#                      verbose = 1,
+#                      refit=True, # 1번
+#                      n_jobs = -1, # 271번 
+#                      n_iter=11, # 디폴트 10
+#                      random_state=33
+
+#                      )
+
+
+
+model = HalvingGridSearchCV(xgb, parameters, cv = kfold, # 54 * 5 = 270번 
+                     verbose = 1,
+                     refit=True, # 1번
+                     n_jobs = -1, # 271번 
+                     random_state=33,
+                     factor = 3, # 배수: min_resources * factor //
+                                 # n_candidates / factor
+                     min_resources = 20, # 1 iter때의 최소 훈련 행의 갯수
+                     max_resources = 521 # 데이터 행의 갯수(n_samples)
+
+                     )
+
+start = time.time()
+
+#3. 훈련
+model.fit(x_train,y_train)
+
+end =time.time()
+
+print('최적의 매개변수 : ',model.best_estimator_)
+print('최적의 파라미터 : ',model.best_params_)
+
+# exit() # Fitting 5 folds for each of 11 candidates, totalling 55 fits
+
+
+#4 평가 에측
+
+print('best_score : ',model.best_score_)
+print('model.score : ',model.score(x_test,y_test))
+
+y_pred= model.predict(x_test)
+print('accuracy_score : ',accuracy_score(y_test,y_pred))
+
+y_pred_best = model.best_estimator_.predict(x_test)
+print('best_acc_score :', accuracy_score(y_test, y_pred_best))
+
+# 위나 아래나 별 차이 없음 쓰고 싶은거 쓰세요;;
+
+print("걸린 시간:",round(end - start,2),'초')
+
+
+path = './_save/m15_cv_results/'
+
+print(pd.DataFrame(model.cv_results_).sort_values(
+    'rank_test_score',
+    ascending = True).to_csv(path + 'm20_00_rs_cv_07.csv',index = False)) 
+
+
+import joblib
+
+joblib.dump(model.best_estimator_,path + 'm20_07_best_model.joblib' )
+
+
+
+# best_score :  0.722063492063492
+# model.score :  0.7480916030534351
+# accuracy_score :  0.7480916030534351
+# best_acc_score : 0.7480916030534351
+# 걸린 시간: 4.6 초
