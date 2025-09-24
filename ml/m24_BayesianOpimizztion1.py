5628af451fe57693c1042baa83a5061274d3ca90
junhyeokk97 <junhyeok97@naver.com>
2025-09-24 18:18:00 +0900

    [114일차] ml / m24_BayesianOpimizztion1.py

---
+++ ml/m24_BayesianOpimizztion1.py
@@ -0,0 +1,20 @@
+param_bouns = {'x1' :(-1,5),
+               'x2' : (0,4)}
+
+def y_function(x1,x2):
+    return -x1 **2 - (x2 -2) **2 + 10
+
+# pip install bayesian-optimization
+
+from bayes_opt import BayesianOptimization
+
+optimizer = BayesianOptimization(
+    f = y_function,
+    pbounds = param_bouns,
+    random_state = 333 
+) 
+
+optimizer.maximize(init_points = 15,
+                   n_iter = 30)
+
+print(optimizer.max)
