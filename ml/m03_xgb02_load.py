d6e2aef0c4ed8c35fdf4cd453ba641d7441e8c34
junhyeokk97 <junhyeok97@naver.com>
2025-09-27 15:50:00 +0900

    [117일차] ml / m03_xgb02_load.py

---
+++ ml/m03_xgb02_load.py
@@ -0,0 +1,34 @@
+import numpy as np
+from sklearn.datasets import load_breast_cancer
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+from xgboost import XGBClassifier
+import joblib
+
+#1. 데이터
+x, y = load_breast_cancer(return_X_y=True)
+
+x_train, x_test, y_train, y_test = train_test_split(x,y,
+                                                    random_state=34,
+                                                    test_size=0.2,
+                                                    stratify=y)
+
+                                                    
+#2. 모델구성, #3. 훈련 - 불러오기
+
+path = './_save/m01_job/'
+model = XGBClassifier()
+model.load(path + 'm03_xgb_save.dat')
+
+#4. 평가, 예측
+results = model.score(x_test, y_test)
+print('점수:' ,results)
+
+y_predict = model.predict(x_test)
+acc = accuracy_score(y_test, y_predict)
+print('acc:', acc)
+
+
+# joblib.dump(model, path + 'm01_joblib_save.joblib')
+# 점수: 0.9210526315789473
+# acc: 0.9210526315789473
