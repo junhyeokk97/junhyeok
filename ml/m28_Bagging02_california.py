e5578c89fe16fb799136cfd5aaaecf19566c9efc
junhyeokk97 <junhyeok97@naver.com>
2025-09-27 11:19:00 +0900

    [117일차] ml / m28_Bagging02_california.py

---
+++ ml/m28_Bagging02_california.py
@@ -0,0 +1,68 @@
+import numpy as np
+import numpy as np
+from sklearn.datasets import fetch_california_housing
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import MinMaxScaler
+from xgboost import XGBRegressor
+from sklearn.metrics import r2_score
+from bayes_opt import BayesianOptimization
+import time
+import warnings
+warnings.filterwarnings('ignore')
+import random
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.ensemble import BaggingRegressor,RandomForestRegressor
+
+seed =333
+random.seed(seed)
+np.random.seed(seed)
+
+from xgboost import XGBRegressor
+from sklearn.datasets import fetch_california_housing
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import MinMaxScaler
+
+x, y = fetch_california_housing(return_X_y=True)
+x_train, x_test, y_train, y_test = train_test_split(x, y, train_size=0.8, random_state=333)
+
+scaler = MinMaxScaler()
+x_train = scaler.fit_transform(x_train)
+x_test = scaler.transform(x_test)
+
+
+#2 모델
+
+# model = DecisionTreeRegressor()
+model = BaggingRegressor(DecisionTreeRegressor(), 
+                         n_estimators = 100,
+                         n_jobs = -1,
+                         random_state = 33,
+                         bootstrap = False
+                         )
+
+
+# model = RandomForestRegressor(random_state=33)
+
+#3 훈련
+model.fit(x_train,y_train)
+
+#4 평가 예측
+results = model.score(x_test,y_test)
+print('최종 점수: ', results)
+
+# DecisionTreeRegressor
+# 최종 점수:  0.5934739286037887
+
+
+
+# BaggingRegressor(DecisionTreeRegressor) -> 부트스트랩 true(디폴트)(샘플 데이터 중복허용)
+# 최종 점수:  0.8038269974176768
+
+
+# BaggingRegressor(DecisionTreeRegressor) -> 부트스트랩 false
+# 최종 점수:  0.6218701133202227
+
+# RandomforestRegressor
+# 최종 점수:  0.8027525299519018
+
+
