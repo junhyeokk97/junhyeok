136db94ca32ce68c8bb11e3839384ae2e5351eaf
junhyeokk97 <junhyeok97@naver.com>
2025-10-13 18:39:00 +0900

    [130일차] ml / m48_Fl_컬럼plus_PCA09_wine.py

---
+++ ml/m48_Fl_컬럼plus_PCA09_wine.py
@@ -0,0 +1,77 @@
+from sklearn.datasets import load_wine
+
+from xgboost import XGBClassifier, XGBRegressor
+import random
+import numpy as np
+from sklearn.model_selection import train_test_split
+import pandas as pd
+from sklearn.decomposition import PCA
+
+seed = 50
+random.seed(seed)
+np.random.seed(seed)
+
+datasets = load_wine()
+x = datasets.data
+y = datasets.target
+
+x_train, x_test, y_train, y_test = train_test_split(x,y,
+                                                    test_size=0.1,
+                                                    random_state=seed,)
+                                                    # stratify=y)
+
+model = XGBRegressor(random_state=seed)
+model.fit(x_train, y_train)
+print("=======", model.__class__.__name__, "=======")
+print('r2: ', model.score(x_test, y_test))
+print(model.feature_importances_)
+
+print(np.percentile(model.feature_importances_, 25))
+
+percentile = np.percentile(model.feature_importances_, 25)
+# print(type(percentile)) <class 'numpy.float64'>
+
+col_name=[]
+for i, fi in enumerate(model.feature_importances_):
+    # print(i, fi)
+    if fi <= percentile:
+        col_name.append(datasets.feature_names[i])
+    else:
+        continue
+print(col_name) # ['AveBedrms', 'Population']
+
+x = pd.DataFrame(x, columns=datasets.feature_names)
+x1 = x.drop(columns=col_name)
+x2 = x[['total_phenols', 'nonflavanoid_phenols', 'proanthocyanins', 'od280/od315_of_diluted_wines']]
+# print(x2)
+
+model.fit(x_train, y_train)
+print('r2_2: ', model.score(x_test, y_test))
+
+x1_train, x1_test, x2_train, x2_test = train_test_split(x1, x2,
+                                                    test_size=0.1,
+                                                    random_state=seed,)
+                                                    # stratify=y)
+
+print(x1_train.shape, x1_test.shape)
+print(x2_train.shape, x2_test.shape)
+print(y_train.shape, y_test.shape)
+
+pca = PCA(n_components=1)
+x2_train = pca.fit_transform(x2_train)
+x2_test = pca.transform(x2_test)
+print(x2_train.shape, x2_test.shape) # (16512, 1) (4128, 1)
+
+x_train = np.concatenate([x1_train, x2_train], axis=1)
+x_test = np.concatenate([x1_test, x2_test], axis=1)
+
+print(x_train, x_test)
+
+model.fit(x_train, y_train)
+print('FI_Drop + PCA: ', model.score(x_test, y_test))
+
+
+
+
+# r2:  0.8226742921600978
+# FI_Drop + PCA:  0.8226363924790552
